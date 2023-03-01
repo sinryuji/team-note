deadf67bac1c6c1ab04e50bfd5508f9f5f358f40
sinryuji <siler2474@gmail.com>
2023-03-01 16:13:45 +0900

    add bfs.py

---
+++ algorithm/searching/bfs.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    # 큐가 빌 때 까지 반복
+    while queue:
+        v = queue.popleft()
+        print(v, end = ' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현
+visited = [False] * 9
+
+# bfs 호출
+bfs(graph, 1, visited)

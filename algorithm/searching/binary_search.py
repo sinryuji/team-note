1651c0267556d4f69355002eb87eb05894355b71
sinryuji <siler2474@gmail.com>
2023-04-19 22:27:33 +0900

    add binary_search.py

---
+++ algorithm/searching/binary_search.py
@@ -0,0 +1,25 @@
+def binary_search(target, data):
+	data.sort()
+	start = 0
+	end = len(data) - 1
+
+	while start <= end:
+		mid = (start + end) // 2
+		
+		if data[mid] == target:
+			return mid
+		elif data[mid] < target:
+			start = mid + 1
+		else:
+			end = mid - 1
+
+	return None
+
+l = [i ** 2 for i in range(11)]
+target = 9
+idx = binary_search(target, l)
+
+if idx:
+    print(l[idx])
+else:
+    print("찾으시는 타겟 {}가 없습니다".forma(target))

00bd2bbb22f9e32a2d42a00d6fab1e962c961fe2
sinryuji <siler2474@gmail.com>
2023-12-05 20:57:33 +0900

    binary_tree.py

---
+++ algorithm/data structure/tree/binary_tree.py
@@ -0,0 +1,35 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.left = None
+
+
+class BinaryTree(object):
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        self.root = self._insert_value(self.root, data)
+        return self.root is not None
+
+    def _insert_value(self, node, data):
+        if node is None:
+            node = Node(data)
+        else:
+            if data <= node.data:
+                node.left = self._insert_value(node.left, data)
+            else:
+                node.right = self._insert_value(node.right, data)
+        return node
+
+    def find(self, key):
+        return self._find_value(self.root, key)
+
+    def _find_value(self, root, key):
+        if root is None or root.data == key:
+            return root is not None
+        elif key < root.data:
+            return self._find_value(root.left, key)
+        else:
+            return self._find_value(root.right, key)

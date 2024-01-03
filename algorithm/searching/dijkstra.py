d17e7aa9eef1fbb667ee0e8a6c99cb07e75db61f
sinryuji <siler2474@gmail.com>
2024-01-03 19:31:28 +0900

    dijkstra.py

---
+++ algorithm/searching/dijkstra.py
@@ -0,0 +1,39 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+INF = int(1e9)
+
+n, m = map(int, input().split())
+start = int(input())
+graph = [[] for _ in range(n + 1)]
+distance = [INF for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append([b, c])
+
+
+def dijkstra(start):
+    q = []
+    distance[start] = 0
+    heapq.heappush(q, (distance[start], start))
+    while q:
+        dist, curr = heapq.heappop(q)
+        if distance[curr] < dist:
+            continue
+        for next_ in graph[curr]:
+            cost = dist + next_[1]
+            if distance[next_[0]] > cost:
+                distance[next_[0]] = cost
+                heapq.heappush(q, (cost, next_[0]))
+
+
+dijkstra(start)
+
+for i in range(1, n + 1):
+    if i == INF:
+        print("INFINITY")
+    else:
+        print(distance[i])

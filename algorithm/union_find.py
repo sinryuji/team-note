63e467412ef2c24cb2c6d4f5763e9008796c498d
sinryuji <siler2474@gmail.com>
2024-02-16 17:37:47 +0900

    union_find.py

---
+++ algorithm/union_find.py
@@ -0,0 +1,34 @@
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = map(int, input().split())
+
+# 부모 테이블 초기화
+parent = [i for i in range(v + 1)]
+
+# Union 연산을 각각 수행
+for i in range(e):
+    a, b = map(int, input().split())
+    union(a, b)
+
+# 각 원소가 속한 집합 출력하기
+print('각 원소가 속한 집합: ', end='')
+for i in range(1, v + 1):
+    print(find(i), end=' ')
+
+print()
+
+# 부모 테이블 내용 출력하기
+print('부모 테이블: ', end='')
+for i in range(1, v + 1):
+    print(parent[i], end=' ')

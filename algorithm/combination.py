a917264f8b57833e7cc5621d082562be553025ce
sinryuji <siler2474@gmail.com>
2024-04-02 17:51:57 +0900

    combination.py

---
+++ algorithm/combination.py
@@ -0,0 +1,20 @@
+def combination(arr, r):
+    arr = sorted(arr)
+    result = []
+
+    def generate(chosen):
+        if len(chosen) == r:
+            result.append(chosen.copy())
+
+        start = arr.index(chosen[-1]) + 1 if chosen else 0
+        for nxt in range(start, len(arr)):
+            chosen.append(arr[nxt])
+            generate(chosen)
+            chosen.pop()
+
+    generate([])
+    return result
+
+arr = [1, 2, 3, 4, 5]
+
+print(*combination(arr, 2), sep="\n")

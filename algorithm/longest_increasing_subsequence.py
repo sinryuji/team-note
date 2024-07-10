d796c78d0ce6043c01f585f5b8f0d6759fda4825
sinryuji <siler2474@gmail.com>
2024-07-10 17:32:52 +0900

    add longest_increasing_subsequence.py

---
+++ algorithm/longest_increasing_subsequence.py
@@ -0,0 +1,18 @@
+N = int(input())
+nums = list(map(int, input().split()))
+dp = [1] * N
+
+for i in range(N):
+    for j in range(i):
+        if nums[i] > nums[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+subsequence = []
+cur = max(dp)
+for i in range(N-1, -1, -1):
+    if dp[i] == cur:
+        subsequence.append(nums[i])
+        cur -= 1
+
+print(max(dp))
+print(*subsequence[::-1])

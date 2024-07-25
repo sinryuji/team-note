0d7a74fd3521dfe48cc72a0ca292cdeaffaec344
sinryuji <siler2474@gmail.com>
2024-07-25 18:05:22 +0900

    update bubble_sort.py selection_sort.py

--- algorithm/sort/bubble_sort.py
+++ algorithm/sort/bubble_sort.py
@@ -3,6 +3,7 @@ def bubble_sort(arr):
         for j in range(len(arr) - i):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
 
-print(bubble_sort([5, 3, 7, 6, 10, 9, 7, 8, 4]))
+arr = [5, 3, 7, 6, 10, 9, 7, 8, 4]
+bubble_sort(arr)
+print(arr)
--- algorithm/sort/selection_sort.py
+++ algorithm/sort/selection_sort.py
@@ -5,6 +5,7 @@ def selection_sort(arr):
             if arr[j] < arr[min_idx]:
                 min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
-    return arr
 
-print(selection_sort([5, 3, 10, 9, 4, 6, 7]))
+arr = [5, 3, 10, 9, 4, 6, 7]
+selection_sort(arr)
+print(arr)
